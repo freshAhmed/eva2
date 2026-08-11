e155de1cd30a18adca422abbe2a0fa690d8e39a7
Ahmed ali <sertyd37@gmail.com>
2026-08-11 17:02:29 -0400

    actulizar la class Database in db.py

--- db.py
+++ db.py
@@ -8,43 +8,55 @@ class Database:
         self.nombre_db=nombre_db if nombre_db else "MovimentosYCtaCte.db"
         self.connexion=sqlite3.connect(self.nombre_db) 
 
-    def crear_tabla(self,nombre_table,columnas=[],tipo_columnas=[],unicos_columnas=[],foreign_keys=[],referencias=[]):
+    def crear_tabla(self,nombre_tabla,columnas=[],tipo_columnas=[],unicos_columnas=[],foreign_keys=[],referencias=[]):
      cursor=self.connexion.cursor()
-     TABLE_CREACIÓN_QUERY=f"""CREATE TABLE IF NOT EXISTS {nombre_table} ({0});""" 
+  
      columnasformateadas=[]
      if len(columnas) == len(tipo_columnas):
       for col, tipo in zip(columnas,tipo_columnas):
-           
-           if col in foreign_keys:
-               columna_index=foreign_keys.index(col)
-               columnasformateadas.append(f"FOREIGN KEY ({col}) REFERENCES {referencias[columna_index]}") 
            if col in unicos_columnas:
             if tipo.upper() =="BIT":
-             columnasformateadas.append(f"{col} INTEGER NOT NULL CHECK (activo IN (0, 1)) PRIMARY KEY AUTOINCREMENT")
+             columnasformateadas.append(f"{col} INTEGER NOT NULL CHECK ({col} IN (0, 1)) PRIMARY KEY")
             else:    
-             columnasformateadas.append(f"{col} {tipo} PRIMARY KEY AUTOINCREMENT")
+             columnasformateadas.append(f"{col} {tipo} PRIMARY KEY")
            else:
             if tipo.upper() =="BIT":
-               columnasformateadas.append(f"{col} INTEGER NOT NULL CHECK (activo IN (0, 1))") 
+               columnasformateadas.append(f"{col} INTEGER NOT NULL CHECK ({col} IN (0, 1))") 
             else:   
-             columnasformateadas.append(f"{col}{tipo}")  
-      cursor.execute(TABLE_CREACIÓN_QUERY.format(",".join(columnasformateadas)))
+             columnasformateadas.append(f"{col} {tipo} ")  
+      for col in columnas:
+            if col in foreign_keys:
+                                  columna_index=foreign_keys.index(col)
+                                  columnasformateadas.append(f"FOREIGN KEY ({col}) REFERENCES {referencias[columna_index]}")    
+      s=" , ".join(columnasformateadas)      
+   
+      TABLE_CREACIÓN_QUERY=f"CREATE TABLE IF NOT EXISTS {nombre_tabla} ({s});" 
+
+      try:
+       cursor.execute(f"DROP TABLE IF EXISTS {nombre_tabla}") 
+       cursor.execute(TABLE_CREACIÓN_QUERY)
+      except sqlite3.OperationalError as e:
+         print (f"error in execute {e}")
       self.connexion.commit()
      else:
         raise ValueError("No SE PUEDE CREAR LA TABLE {nombre_table} EN DATABASE {self.nombre_db} PORQUE EL NUMERO DE COLUMNAS NO COINCIDE CON EL NUMERO DE TIPOS PRESENTADOS ") 
      
     
 
-    def insertar_datos(self,nombre_table,columnas=[],valores=[]):
+    def insertar_datos(self,nombre_tabla,columnas=[],valores=[]):
        cursor=self.connexion.cursor();
+       valores=list(map(str,valores))
+       espaciostr=",".join(["?"]*len(valores))
        if len (valores)>0 and len(columnas)==0:  
-        INSERT_QUERY=f"""INSERT INTO {nombre_table} VALUES ({1})"""
-        cursor.execute(INSERT_QUERY.format(",".join(valores)))
+       
+        INSERT_QUERY=f"""INSERT INTO {nombre_tabla} VALUES ({espaciostr})"""
+        cursor.execute(INSERT_QUERY,valores)
         self.connexion.commit()
 
-       if len(valores)==len(columnas):
-          INSERT_QUERY=f"""INSERT INTO {nombre_table} ({0}) VALUES ({1})"""
-          cursor.execute(INSERT_QUERY.format(",".join(columnas),",".join(valores)))
+       elif len(valores)==len(columnas):
+          
+          INSERT_QUERY=f"""INSERT INTO {nombre_tabla} ({",".join(columnas)}) VALUES ({espaciostr})"""
+          cursor.execute(INSERT_QUERY,valores)
           self.connexion.commit()
        else:
           raise ValueError("NO SE PUEDE INSERTAR LOS DATOS EN EL DATABASE")  
@@ -52,12 +64,11 @@ class Database:
     def buscar_datos(self,nombre_table,columnas=[],condicion=""):
         cursor=self.connexion.cursor()
         datos=[]
-        if len(columnas)==0:
-           SELECT_QUERY=f"""SELECT * FROM {nombre_table} WHERE {condicion};"""
-           datos=cursor.execute(SELECT_QUERY).fetchall()
-        else:
-           SELECT_QUERY=f"""SELECT {0} FROM {nombre_table} WHERE {1};"""
-           datos=cursor.execute(SELECT_QUERY.format(",".join(columnas),condicion)).fetchall()
+        SELECT_QUERY=f"""SELECT * FROM {nombre_table} WHERE {condicion}""" if len(condicion)>0 else f"""SELECT * FROM {nombre_table}"""
+        if len(columnas)>0:
+           SELECT_QUERY=f"""SELECT {",".join(columnas)} FROM {nombre_table} WHERE {condicion}""" if len(condicion)>0 else f"""SELECT {",".join(columnas)} FROM {nombre_table}"""
+       
+        datos=cursor.execute(SELECT_QUERY).fetchall()
         return datos 
 
     def eliminar_datos(self,nombre_table,condicion=""):
@@ -72,9 +83,8 @@ class Database:
     def actualizar_datos(self,nombre_table,columnas=[],valores=[],condicion=""):
        cursor=self.connexion.cursor()
        if len(valores)==len(columnas):
-          ACTUALIZAR_QUERY=f"""UPDATE {nombre_table} SET {0} WHERE {1};"""
-          cursor.execute(ACTUALIZAR_QUERY.format(
-             (f"{0}={1}".format(col,val) for col,val in zip(columnas,valores)),condicion))
+          ACTUALIZAR_QUERY=f"""UPDATE {nombre_table} SET {(f"{col}={val}" for col,val in zip(columnas,valores))} WHERE {condicion};"""
+          cursor.execute(ACTUALIZAR_QUERY)
           self.connexion.commit()
        else:
           raise ValueError("NO SE PUEDE ACTUALIZAR LOS DATOS EN EL DATABASE")  
