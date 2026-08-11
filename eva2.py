21d6794e8c209d8563c1c018aa06e91a9a3e41b5
Ahmed ali <sertyd37@gmail.com>
2026-08-11 18:37:05 -0400

    agregar nuevos metodos abonar y cargar y exportar_archivos a la class Cuenta_Corriente

--- eva2.py
+++ eva2.py
@@ -1,34 +1,7 @@
 from db import Database
+import csv
 
 
-try:
-#iniciar la base de datos
- db=Database("MovimentosYCtaCte.db")
-#crear la tabla CtaCte
- nombre_tabla="CtaCte"
- columnas=["numeroCtaCte","rutTitularCta","nomTitularCta","saldoCta"]
- tipo_columnas=["DOUBLE","VARCHAR(12)","VARCHAR(105)","DOUBLE"]
- unicos_columnas=["numeroCtaCte"]
- foreign_keys=["numeroCtaCte"]
- referencias=["Movimientos(numeroCtaCte)"]
- db.crear_tabla(nombre_tabla,columnas,tipo_columnas,unicos_columnas,foreign_keys,referencias)
-
-
-
-#crear la tabla Movimientos
- nombre_tabla="Movimientos"
- columnas=["CtaCte_numeroCtaCte","idMovimientos","tipoMovimiento","monto"]
- tipo_columnas=["DOUBLE","DOUBLE","BIT","DOUBLE"]
- unicos_columnas=["idMovimientos"]
-
-    
- db.crear_tabla(nombre_tabla,columnas,tipo_columnas,unicos_columnas)
-
- db.insertar_datos("CtaCte",[],[12341232,"12360322-2","johan",12312.2])
- datos=db.buscar_datos("CtaCte") # buscar todas los datos
- print(datos)
-except ValueError as e :
-   print(e)
 class Cuenta_Corriente:
     """
     Clase que representa una cuenta corriente.
@@ -47,3 +20,41 @@ class Cuenta_Corriente:
         pass
     def cargar (self,monto):
        pass       
+    def exportar_registros(self,nombre_tabla):
+       datos=self.db.buscar_datos(nombre_tabla)
+       nombres_columnas=[descripción[0] for descripción in self.db.get_columnas("CtaCte")]
+       with open(f"{nombre_tabla}.csv","w",newline="",encoding="utf-8") as archivo:
+         escritor=csv.writer(archivo)
+         escritor.writerow(nombres_columnas)
+         escritor.writerows(datos)
+       print("CVS archivo creado correctamente") 
+
+if "__main__" == __name__:
+ try:
+#iniciar la base de datos
+  db=Database("MovimentosYCtaCte.db")
+#crear la tabla CtaCte
+  nombre_tabla="CtaCte"
+  columnas=["numeroCtaCte","rutTitularCta","nomTitularCta","saldoCta"]
+  tipo_columnas=["DOUBLE","VARCHAR(12)","VARCHAR(105)","DOUBLE"]
+  unicos_columnas=["numeroCtaCte"]
+  foreign_keys=["numeroCtaCte"]
+  referencias=["Movimientos(numeroCtaCte)"]
+  db.crear_tabla(nombre_tabla,columnas,tipo_columnas,unicos_columnas,foreign_keys,referencias)
+
+
+
+#crear la tabla Movimientos
+  nombre_tabla="Movimientos"
+  columnas=["CtaCte_numeroCtaCte","idMovimientos","tipoMovimiento","monto"]
+  tipo_columnas=["DOUBLE","DOUBLE","BIT","DOUBLE"]
+  unicos_columnas=["idMovimientos"]    
+  db.crear_tabla(nombre_tabla,columnas,tipo_columnas,unicos_columnas)
+  db.insertar_datos("CtaCte",[],[12341232,"12360322-2","johan",12312.2])
+
+ except ValueError as e :
+   print(e)
+ CtaCte=Cuenta_Corriente(db,22341232,"12360322-2","johan",12312.2)  
+ CtaCte.exportar_registros("CtaCte")
+ datos=db.buscar_datos("CtaCte") # buscar todas los datos
+ print(datos) 
